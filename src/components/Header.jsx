75e3e4e4b0387eff7af92d1d0e55a7589d209d43
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2022-06-16 20:15:22 -0300

    global.css and Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import styles from './Header.module.css'
+import igniteLogo from '../assets/ignite-logo.svg'
+
+console.log(igniteLogo)
 
 export function Header(){
     return(
         <header className={styles.header}>
-            <strong>Ignite Feed</strong>
+            <img src={igniteLogo} alt="logotipo do Ignite" />
         </header>
     );
 }
