b8fddb1974e769aa1e423b25ca3aa0165fb0a8bf
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2022-06-20 01:15:01 -0300

    Final Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,19 +1,30 @@
 import styles from './Sidebar.module.css';
+import { PencilLine } from 'phosphor-react'
 
 export function Sidebar() {
     return (
         <aside className={styles.sidebar}>
             <img
                 className={styles.cover}
-                src="https://images.unsplash.com/photo-1639149888905-fb39731f2e6c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=50" />
+                src="https://images.unsplash.com/photo-1509343256512-d77a5cb3791b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=50" />
 
             <div className={styles.profile}>
+
+            <img
+            className={styles.avatar} 
+            src="https://avatars.githubusercontent.com/u/61327251?v=4" 
+            alt="Avatar do usuário" />
+
                 <strong>Luís Henrique</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
-                <a href="#">Editar seu perfil</a>
+                
+                <a href="#">
+                <PencilLine size={20}/>
+                    Editar seu perfil
+                </a>
             </footer>
 
         </aside>
