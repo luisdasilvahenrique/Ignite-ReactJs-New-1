da26aa9ba70bd814cf0509f142fa947b328e5489
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2022-06-21 11:00:20 -0300

    Component: Post and Post.module.css

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -15,15 +15,19 @@ export function Post(){
                 <time title='20 de Junho às 11:47' dateTime='2022-05-20 11:47:33'>Publicado há 1h</time>
             </header>
 
-            <div className={styles.content}>
+                <div className={styles.content}>
             <p>Fala galeraa 👋</p> 
 
             <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
 
             <p>👉 <a href="#">jane.design/doctorcare</a> </p>
 
-            <p> <a href="#">#novoprojeto #nlw #rocketseat</a> </p>
-            </div>
+            <p> 
+            <a>#novoprojeto</a> 
+            <a>#nlw </a>
+            <a>#rocketseat</a>
+            </p>
+                </div>
         </article>
     );
 }
