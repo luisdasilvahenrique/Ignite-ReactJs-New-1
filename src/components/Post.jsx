b4e24123f9890a86095cfa6e0b3dfaf3e8b493e9
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2022-06-21 22:00:53 -0300

    Form: Comment

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -23,11 +23,25 @@ export function Post(){
             <p>👉 <a href="#">jane.design/doctorcare</a> </p>
 
             <p> 
-            <a>#novoprojeto</a> 
-            <a>#nlw </a>
-            <a>#rocketseat</a>
+            {' '}<a>#novoprojeto</a> 
+            {' '}<a>#nlw </a>
+            {' '}<a>#rocketseat</a>
             </p>
                 </div>
+
+                <form className={styles.commentForm}>
+                    <strong>Deixe seu feedback</strong>
+
+                    <textarea 
+                    placeholder='Deixe um comentário'
+                    />
+
+                    <footer>
+                        <button type='submit'>
+                            Publicar
+                        </button>
+                    </footer>
+                </form>
         </article>
     );
 }
