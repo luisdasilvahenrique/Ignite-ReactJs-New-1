b53e06ba9c3d4e0abb47a81e149f23160b57f5c4
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2022-06-23 23:16:44 -0300

    Component: Avatar

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,12 +1,13 @@
 import styles from './Post.module.css';
 import { Comment } from './Comment';
+import { Avatar } from './Avatar';
 
 export function Post(){
     return(
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img className={styles.avatar} src="https://github.com/luisdasilvahenrique.png" />
+                    <Avatar src="https://github.com/luisdasilvahenrique.png" />
                     <div className={styles.authorInfo}>
                         <strong>Luis Henrique</strong>
                         <span>Web Developer</span>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import styles from './Sidebar.module.css';
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar';
 
 export function Sidebar() {
     return (
@@ -10,10 +11,7 @@ export function Sidebar() {
 
             <div className={styles.profile}>
 
-            <img
-            className={styles.avatar} 
-            src="https://avatars.githubusercontent.com/u/61327251?v=4" 
-            alt="Avatar do usuário" />
+            <Avatar src="https://avatars.githubusercontent.com/u/61327251?v=4"/>
 
                 <strong>Luís Henrique</strong>
                 <span>Web Developer</span>
