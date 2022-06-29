da6b6164422e880ad12094470f1e399ece03c4c4
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2022-06-29 10:43:06 -0300

    Iterando no JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,50 @@
 import { Header } from './components/Header';
 import { Sidebar } from './components/Sidebar';
 import { Post } from './components/Post';
- 
+
 import styles from './App.module.css';
 
 import './global.css'
+
+// author: { avatar_url:"", name: "", rule: ""}
+// publishedAt : Date
+// content: String
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/luisdasilvahenrique.png',
+      name: 'Luis Henrique',
+      rule: 'Web Developer'
+    },
+    content: [
+
+      { type: 'paragraph', content: 'Fala galeraa,' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+
+    ],
+    publishedAt: new Date('2022-06-29 10:19:42'),
+  },
+
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      rule: 'Sênior Web Dev & Instructor'
+    },
+    content: [
+
+      { type: 'paragraph', content: 'Fala galeraa,' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+
+    ],
+    publishedAt: new Date('2022-05-10 01:19:42'),
+  },
+];
 
 export function App() {
   return (
@@ -16,15 +56,15 @@ export function App() {
         <Sidebar />
 
         <main>
-          <Post
-            author="Luis Henrique"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum quisquam nam eaque molestiae consectetur esse maiores placeat? Officia dignissimos, voluptates nostrum quos, vero recusandae harum quod sed libero alias deleniti!"
-          />
-
-          <Post
-            author="João Gabriel"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum quisquam nam eaque molestiae consectetur esse maiores placeat? Officia dignissimos, voluptates nostrum quos, vero recusandae harum quod sed libero alias deleniti!"
-          />
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
